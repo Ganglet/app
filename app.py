272c63351c1241a5f77065be765ba74b447a6eca
Ganglet <angshumanchakravertty2@gmail.com>
2024-12-13 23:24:09 +0530

    Pushing the Flask files

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+#creates WSGI
+app=Flask(__name__)
+
+#when we press / goes to yokoso page (should be home), default is also that page
+@app.route("/")
+def Welcome():
+    return "Welcome to the page"
+
+@app.route("/index")
+def index():
+    return "This is the index page"
+
+@app.route("test")
+def test():
+    return "This is a test page"
+
+if __name__=="__main__":
+    #debug=True makes all changes live u only need to restart
+    app.run(debug=True)
